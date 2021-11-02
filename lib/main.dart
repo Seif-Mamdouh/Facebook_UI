8f308d1495ba24c6475fba079256d7228e9fcfe8
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2021-11-02 16:42:15 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'package:project/screens/conversations.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Messenger Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const Conversations(),
+    );
+  }
+
+  
+}
+
+
